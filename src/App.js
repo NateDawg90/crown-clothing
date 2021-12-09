a6bfb2bacee722aee2fc632fb0c742f898baa8f9
Nathan Johnson <rnjclemson@gmail.com>
2021-12-08 18:06:27 -0600

    add header compt and logo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
 
 export default function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
